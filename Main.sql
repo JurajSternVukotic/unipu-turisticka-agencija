3c8f342a81352358b5d4158a74ca4f14bdaa4624
JurajSternVukotic <jsternvuk@unipu.hr>
2023-05-23 18:07:57 +0200

    refactor most of my code, add constraints and comments

--- Main.sql
+++ Main.sql
@@ -56,62 +56,71 @@ CREATE TABLE Uplata (
 
 -- Jurjev dio
 CREATE TABLE Grad(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    naziv VARCHAR(64),
-    opis TEXT(500),
-    Postanski_Broj VARCHAR(32)
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
+    naziv VARCHAR(64) NOT NULL, # Najduzi naziv grada na svijetu ima 58 znakova, 64 bi trebalo biti dovoljno
+    opis TEXT(500), # 500 znakova bi trebalo biti dovoljno za opis da ne bude predug
+    Postanski_Broj VARCHAR(32) NOT NULL UNIQUE # Postanski brojevi mogu imati i slova, u nasem modelu jedan grad ima jedan postanski broj za razliku od inace gdje svaka ulica moze imati u nekim drzavama
 );
 
 CREATE TABLE Adresa(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	naziv VARCHAR(64)
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
+	naziv_ulice VARCHAR(128) NOT NULL, # 128 bi trebalo biti dovoljno za bilo koju ulicu
+    broj_ulice INT NOT NULL, # Ulica mora imati broj
+	CHECK (broj_ulice > 0), # broj ulice mora biti strogo veci od 0
+    dodatan_opis TEXT(256) # Dodatne informacije o kako doci do ulice, koji kat, itd.
 );
 
-CREATE TABLE Grad_Adrese(
-	id_grad INT NOT NULL REFERENCES Grad (id) ON DELETE CASCADE,
+CREATE TABLE Grad_Adrese( # povezuje gradove sa adresama, ako nestane grad ili adresa se ovo brise
+	id_grad INT NOT NULL REFERENCES Grad (id) ON DELETE CASCADE, 
     id_adresa INT NOT NULL REFERENCES Adresa (id) ON DELETE CASCADE
 );
 
 CREATE TABLE Drzava(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    naziv VARCHAR(64),
-    opis TEXT(500),
-    valuta VARCHAR(32),
-    cijena NUMERIC(10, 2),
-    dokumenti_za_ulaz TEXT(500),
-    jezik VARCHAR(32),
-    pozivni_broj INT
-);
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
+    naziv VARCHAR(64) NOT NULL UNIQUE, # Najduzi naziv drzave je 56, tako da bi ovo trebalo biti dovoljno, ime mora biti jedinstveno
+    opis TEXT(500), # 500 znakova bi trebalo biti dovoljno za opis da ne bude predug
+    valuta VARCHAR(32) NOT NULL, # Ime valute koja se koristi
+    tecaj_u_eurima NUMERIC(10, 2) NOT NULL, # Koliko je jedan euro vrijedan ove valute 
+    dokumenti_za_ulaz TEXT(500), # Kratki opis kakva je trenutna procedura za ulazak u drzavu, kasnije se moze dodati tablica koja gleda relaciju izmedju svake dvije drzave
+    jezik VARCHAR(32) NOT NULL, # Naziv jezika koji se prica
+    pozivni_broj INT NOT NULL UNIQUE, # pretpostavlja se da se ne pise niti 00 niti + izmedju posto je to preferenca formatiranja, takodjer da nema crtice nego samo se nastavi pisati posto je nepotrebno tako da moze biti INT, dvije drzave ne mogu imati isti pozivni broj
+	CHECK (pozivni_broj > 0) # pozivni broj ne moze biti negativan niti nula
+);	
 
-CREATE TABLE Drzava_Grada(
+CREATE TABLE Drzava_Grada( # povezuje gradove sa drzavama, ako nestane grad ili drzava se ovo brise (nuklarna eksplozija)
 	id_Drzava INT NOT NULL REFERENCES Drzava (id) ON DELETE CASCADE,
     id_Grad INT NOT NULL REFERENCES Grad (id) ON DELETE CASCADE
 );
 
-CREATE TABLE Stavka_Korisnicke_Podrske(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Stavka_Korisnicke_Podrske( #support ticket
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
     # Mozda treba napraviti specijalnu tablicu sa id stavke, id korisnika i id zaposnelika???
-	id_korisnik INT NOT NULL REFERENCES Korisnik (id) ON DELETE CASCADE,
-    id_zaposlenik INT NOT NULL REFERENCES Zaposlenik (id) ON DELETE CASCADE,
-    vrsta_problema ENUM ('Placanje', 'Rezervacija', 'Problemi sa zaposlenicima', 'Tehnicki problemi', 'Povrat novca', 'Drugo'),
-    opis_problema TEXT (500),
-    status_problema ENUM('Zaprimljeno', 'U obradi', 'Na cekanju', 'Rjeseno')
+	id_korisnik INT NOT NULL REFERENCES Korisnik (id), # zelimo znati koji korisnik je zatrazio podrsku, i da to ostane cak i ako korisnika vise nema
+    id_zaposlenik INT NOT NULL REFERENCES Zaposlenik (id), # zelimo uvijek imati tocno jednu osobu koja radi na ovoj podrsci, i da ostane cak i ako taj zaposlenik ode 
+    vrsta_problema ENUM ('Placanje', 'Rezervacija', 'Problemi sa zaposlenicima', 'Tehnicki problemi', 'Povrat novca', 'Drugo') NOT NULL, # ticket podrske moze biti samo jedna od ovih stvari
+    opis_problema TEXT (2500), # opis problema mora biti teksutalan i imati manje od 2500 znakova
+    CHECK (LENGTH(opis_problema) >= 50), # opis mora imati vise od 49 znakova kako bi smanjili zlouporabu sa praznim ili nedostatnim opisima 
+	status_problema ENUM('Zaprimljeno', 'U obradi', 'Na cekanju', 'Rjeseno') NOT NULL # svaki ticket ima svoje stanje, ovisno u kojoj fazi proceisranja je
 );
 
 CREATE TABLE Paket(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    naziv VARCHAR (64),
-    opis TEXT (1000),
-    min_ljudi INT,
-    max_ljudi INT,
-    popunjenih_mjesta INT,
-    pocetak_puta DATE,
-    kraj_puta DATE,
-    cijena_po_turistu DECIMAL(12,2)
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
+    naziv VARCHAR (64) NOT NULL UNIQUE, # unique kako ne bi imali duplikatne nazive paketa, 64 znakova bi trebalo biti dovoljno za naslov 
+    opis TEXT (1000), # 1000 znakova za opis paketa, ako se zele detaljne informacije moze se poslati upit za putni plan
+    min_ljudi INT NOT NULL DEFAULT 1, # minimalno ljudi koliko je potrebno da se odrzi putovanje 
+    max_ljudi INT NOT NULL DEFAULT 1, # maksimalno ljudi koji mogu sudjelovati na putovanju
+    CHECK (min_ljudi >= 1), # ne mozemo imati paket za manje od jednu osobu
+    CHECK (max_ljudi >= min_ljudi), # maksimalan broj ljudi mora biti jednak ili veci od minimalnog
+    popunjenih_mjesta INT NOT NULL DEFAULT 0,
+    CHECK (popunjenih_mjesta < max_ljudi), # ne moze biti vise popunjenih mjesta od max
+    CHECK (popunjenih_mjesta >= 0), # ne moze biti negativno ljudi
+    pocetak_puta DATETIME NOT NULL, # datum i vrijeme kada put pocinje 
+    kraj_puta DATETIME NOT NULL, # datum i vrijeme kada put zavrsava
+    cijena_po_turistu NUMERIC(10, 2) NOT NULL # koliko kosta za jednu osobu paket
 );
 
 CREATE TABLE Putni_Plan_Stavka(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY, # ID je numericki, sam se povecava kako ne bi morali unositi uvijek, te nam je to primarni kljuc uvijek
     id_paket INT NOT NULL REFERENCES Paket (id) ON DELETE CASCADE,
     id_transport INT NOT NULL REFERENCES transport (id) ON DELETE CASCADE,
     id_odrediste INT NOT NULL REFERENCES odrediste (id) ON DELETE CASCADE,
