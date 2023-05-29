44b7aba6cf53e08f97455c7c2b462c2a5dbf47f4
KarloBazina <karlobazina2@gmail.com>
2023-05-29 11:05:25 +0200

    Neki upiti - Karlo

--- Main.sql
+++ Main.sql
@@ -391,6 +391,18 @@ ON grad_id = adresa.id_grad;
 
 -- Autor: Karlo Bazina
 
+-- osoba XX se zeli uzivo naci u popodnevnim satima sa nasim zaposlenikom
+-- Pronađi sve zaposlenike 'turistički agent', koji XX datuma rade 'popodnevna smjenu' i nalaze se u gradu gdje je osoba XX, 
+-- te ih poredaj po veličini prihoda (pretpostavka je da je najbolji zaposelnik najplaćeniji) 
+
+-- pronadi sve zaposelnika koji rade 'na odredeno' ili 'na neodredeno' te svima koji imaju vise od 5 ocijena, a prosijek ocjena je veci od 4,5 povecaj placu za 10%
+
+-- predstavljanje kompanije je
+-- prikazi mi samo zaposelnika sa najvecom placom iz gradova (Zagreb, Split, Rijeka, Osijek) 
+
+
+
+
 -- Odjeljak VRIJEDNOSTI
 
 /*
@@ -439,7 +451,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\r\n';
 
-
+SELECT * FROM adresa;
 
 SELECT * FROM drzava_grad_adresa WHERE drzava_naziv = 'Austria';
  
