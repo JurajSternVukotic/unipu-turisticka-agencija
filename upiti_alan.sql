5cb051886731242370309133157f30fd087d1cd5
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2023-06-01 21:15:55 +0200

    Update triggers, views and queries

--- upiti_alan.sql
+++ upiti_alan.sql
@@ -1,98 +1,61 @@
 USE turisticka_agencija;
 
--- Autor: Alan Burić
-
--- Testni primjer:
-INSERT INTO kupon VALUE(NULL, 'Čščđćž32', MAKEDATE(2023, 100), MAKEDATE(2023, 140), 20, 1);
-
 ### Alan Burić ###
--- POGLEDI - pohranjeni upiti
 
--- Prikazuje sve IDjeve zaposlenika koji su putni agenti.
-CREATE VIEW svi_putni_agenti AS 
-	SELECT id_zaposlenik AS id 
-    FROM pozicija_zaposlenika 
-    WHERE id_pozicija = (SELECT id 
-						FROM pozicija 
-                        WHERE ime_pozicije = 'putni agent');
-/*
- * I. Pronađi ID pozicije 'putni agent'
- * II. Pronađi sve zaposlenike s tom pozicijom (preko IDja)
- * III. Pobroji njihova pojavljivanja
- * IV. Sortiraj ih od najmanjeg prema najvećem
- */
+-- Testni primjeri:
+INSERT INTO kupon VALUE(NULL, 'Čščđćž32', MAKEDATE(2023, 100), MAKEDATE(2023, 140), 20, 1);
+SELECT * FROM kupon;
 
- CREATE VIEW zaposlenost_rezervacije AS 
- 	SELECT *, COUNT(*) AS kolicina_posla 
- 		FROM svi_putni_agenti 
- 			LEFT JOIN 
- 	(SELECT zaposlenik_id AS id FROM rezervacija) AS rezervacija 
- 			USING (id);
+-- Pogled rezervacija i kupona koji su primjenjeni na njih s iznosom i vrsti kupona
+CREATE VIEW kuponi_rezervacije AS SELECT * FROM kupon JOIN kupon_rezervacija USING (id_kupon);
+            
+SELECT * FROM kuponi_rezervacije;
 
--- OKIDAČI - event handlers
+-- Pogled cijena rezervacija prema njihovim paketima
+CREATE VIEW rezervacija_paket_cijena AS SELECT * FROM (SELECT id AS id_paket, cijena_po_turistu AS cijena FROM paket) AS paket JOIN (SELECT id AS id_rezervacija, id_paket FROM rezervacija) AS rezervacija USING (id_paket);
 
- -- Nužno je za razlikovanja završetka naredbe.
- DELIMITER //
+SELECT * FROM rezervacija_paket_cijena;
 
-CREATE TRIGGER ogranicenje_putnog_agenta BEFORE INSERT ON rezervacija
-	FOR EACH ROW
-		BEGIN
-			DECLARE poruka VARCHAR(128);
-         
- 			IF NEW.zaposlenik_id NOT IN (svi_putni_agenti) THEN
- 				SET poruka = CONCAT('Zadani zaposlenik s IDjem ', NEW.zaposlenik_id, ' nije putni agent za rezervacije!');
- 				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = poruka;
- 			END IF;
- 		END//
+-- 1. Pronađite prave iznose cijena rezervacija s obzirom na primjenu kupon koda prema formuli novi_iznos = max(0, (cijena - SUM(ne postotnih kupona)) * max(1, 1 - SUM(postotnih kupona) / 100)
+CREATE VIEW cijene_rezervacija AS SELECT id_rezervacija, 
+			GREATEST(0, 
+				cijena - IFNULL((SELECT SUM(iznos) 
+								FROM kuponi_rezervacije AS kr 
+								WHERE NOT postotni AND kr.id_rezervacija = rpc.id_rezervacija),
+								0) 
+            * GREATEST(1, 
+				IFNULL(1 - (SELECT SUM(iznos) 
+							FROM kuponi_rezervacije AS kr 
+                            WHERE postotni AND kr.id_rezervacija = rpc.id_rezervacija) / 100, 1))) 
+			AS cijena
+	FROM rezervacija_paket_cijena AS rpc;
 
-/*
-* Ukoliko se zaposlenika otpusti, prema ON DELETE SET NULL ograničenju atributa zaposlenik_id u
-* relaciji rezervacije će atribut s IDjem otpuštenog zaposlenika postati NULL. Potrebno je postaviti novog
-* zaposlenika na tu poziciju kako bi korisnici imali zaposlenika turističke agencije kojeg mogu kontaktirati
-* u vezi s njihovom rezervacijom.
-*/
-CREATE TRIGGER postavi_novog_zaposlenika BEFORE UPDATE ON rezervacija
-	FOR EACH ROW
-		BEGIN
-			IF NEW.zaposlenik_id IS NULL THEN
-				SET NEW.zaposlenik_id = (SELECT * FROM zaposlenost_rezervacije GROUP BY id ORDER BY kolicina_posla ASC LIMIT 1);
-			END IF;
-		END//
+SELECT * FROM cijene_rezervacija;
 
--- 1. Pronađite prave iznose cijena rezervacija s obzirom na primjenu kupon koda prema formuli novi_iznos = max(0, (cijena - SUM(ne postotnih kupona)) * min(0, 1 - SUM(postotnih kupona) / 100)
-SELECT id, 
-			GREATEST(0, (cijena 
-				- IFNULL((SELECT SUM(iznos) 
-								FROM kuponi_rezervacije 
-                                WHERE NOT postotni AND id_rezervacija = id 
-                                GROUP BY id_kupon), 0))
-				* LEAST(0, IFNULL((SELECT SUM(iznos) 
-								FROM kuponi_rezervacije 
-                                WHERE postotni AND id_rezervacija = id 
-                                GROUP BY id_kupon), 0) / 100)) AS novi_iznos
+-- 2. Prikažite sve potpuno plaćene rezervacije
+SELECT * FROM 
+		(SELECT id_rezervacija, SUM(iznos) AS uplaceno 
+			FROM uplata 
+			GROUP BY id_rezervacija) 
+			AS uplata 
+	JOIN 
+		(SELECT id_rezervacija, cijena 
+        FROM cijene_rezervacija) 
+        AS cr
+	JOIN 
+		(SELECT id AS id_rezervacija, id_osoba, id_paket, id_zaposlenik, vrijeme 
+        FROM rezervacija) 
+        AS r
+	USING (id_rezervacija) 
+    HAVING uplaceno >= cijena;
+
+-- 4. Pronađite najčešće korištene kupone sortirane prema padajućemu poretku s novim stupcem "kolicina"
+SELECT id_kupon, kod, datum_pocetka, datum_kraja, iznos, postotni, COUNT(*) AS kolicina 
 	FROM 
-		rezervacija 
-			JOIN 
-        (SELECT id AS id_paket, cijena_po_turistu AS cijena FROM paket) AS paket 
-	USING (id_paket);
-
--- 2. Pronađite sve id_rezervacija za potpuno plaćene rezervacija
-CREATE VIEW placene_rezervacije AS 
-	SELECT id_rezervacija, p.id_paket, p.cijena, SUM(iznos) AS iznos 
-		FROM 
-			(SELECT id AS id_paket, cijena_po_turistu AS cijena FROM paket) AS p,
-			(SELECT id AS id_rezervacija, id_paket FROM rezervacija) AS r 
-				JOIN 
-			uplata USING (id_rezervacija) 
-		WHERE p.id_paket = r.id_paket
-		GROUP BY id_rezervacija
-		HAVING iznos >= cijena;
-
--- 4. Pronađite najviše korištene kupone s nazivom koda prema padajućemu poretku
-SELECT id_kupon, kod, COUNT(*) AS kolicina 
-	FROM kupon_rezervacija 
-		JOIN (SELECT id AS id_kupon, kod FROM kupon) AS k 
-    USING (id_kupon) 
+		kupon_rezervacija 
+        JOIN 
+        kupon 
+		USING (id_kupon) 
     GROUP BY id_kupon 
     ORDER BY kolicina DESC;
 
