bfe7478edbfbf6b6d2aee3547e086cc6ee80791a
KarloBazina <karlobazina2@gmail.com>
2023-06-01 17:20:50 +0200

    Karlo i Mateo upiti

--- upiti_karlo.sql
+++ upiti_karlo.sql
@@ -1,15 +1,61 @@
-USE turisticka_agencija;
+USE  turisticka_agencija;
 
 -- Autor: Karlo Bazina
 
-### Karlo Bazina ###
--- osoba XX se zeli uzivo naci u popodnevnim satima sa nasim zaposlenikom
--- Pronađi sve zaposlenike 'turistički agent', koji XX datuma rade 'popodnevna smjenu' i nalaze se u gradu gdje je osoba XX, 
--- te ih poredaj po veličini prihoda (pretpostavka je da je najbolji zaposelnik najplaćeniji) 
 
--- pronadi sve zaposelnika koji rade 'na odredeno' ili 'na neodredeno' te svima koji imaju vise od 5 ocijena, a prosijek ocjena je veci od 4,5 povecaj placu za 10%
+-- Prikaz svih zaposlenika koji rade na određeno ili na neodređeno koji imaju više od dvije recenzije,
+-- a prosjek ocjena im je 4.5 ili veći, te smo im uvećali plaću za 10 %
 
--- predstavljanje kompanije je
--- prikaži mi samo zaposelnika sa najvecom placom iz gradova (Zagreb, Split, Rijeka, Osijek) 
 
--- prikaži mi 3 zaposelnika koja pričaju najviše stranih jezika, a jedan od tih jezikam ora biti njemački
+CREATE VIEW pogled as
+	SELECT o.puno_ime, count(r.ocjena) AS broj, AVG(r.ocjena) AS pros, z.placa
+	FROM zaposlenik z JOIN osoba o ON z.id=o.id 
+	JOIN recenzija_zaposlenika rz ON z.id=rz.id_zaposlenik
+	JOIN recenzija r ON r.id=rz.id_recenzija
+	WHERE z.ugovor_o_radu='na određeno' OR z.ugovor_o_radu='na neodređeno'
+	GROUP BY o.puno_ime, z.placa
+	ORDER BY pros DESC
+;
+
+SELECT *, placa*1.1 AS uvecana
+FROM pogled
+WHERE broj>=2 AND pros>=4.5
+;
+
+
+-- Prikaži mi zaposlenika sa največom plaćom iz Zagreba, Osijeka, Splita i Rijeke
+
+
+SELECT z.id, o.puno_ime, z.placa, g.naziv AS grad
+FROM zaposlenik z
+JOIN osoba o ON z.id = o.id
+JOIN adresa a ON o.id_adresa = a.id
+JOIN grad g ON a.id_grad = g.id
+WHERE g.naziv IN ('Osijek', 'Zagreb', 'Split', 'Rijeka')
+AND z.placa = (
+  SELECT MAX(placa)
+  FROM zaposlenik z2
+  JOIN osoba o2 ON z2.id = o2.id
+  JOIN adresa a2 ON o2.id_adresa = a2.id
+  JOIN grad g2 ON a2.id_grad = g2.id
+  WHERE g2.naziv = g.naziv
+)
+ORDER BY placa DESC
+;
+
+
+-- Osoba traži putnog agenta koji 1.6. radi popodnevnu smjenu, i može se naći uživo sa njim u Bilbau
+
+
+SELECT o.puno_ime, z.placa, g.naziv,rs.datum, rs.smjena
+FROM zaposlenik z JOIN osoba o ON z.id=o.id 
+JOIN adresa a ON o.id_adresa=a.id
+JOIN grad g ON a.id_grad=g.id
+JOIN radna_smjena rs ON rs.id_zaposlenik=z.id
+JOIN pozicija_zaposlenika pz ON pz.id_zaposlenik=z.id
+JOIN pozicija p ON p.id=pz.id_pozicija
+WHERE rs.datum = '2023-06-01' AND rs.smjena = 'popodnevna' AND p.ime_pozicije ='putni agent' AND g.naziv ='Bilbao'
+ORDER BY z.placa DESC 
+LIMIT 1
+;
+
--- upiti_mateo.sql
+++ upiti_mateo.sql
@@ -1,4 +1,63 @@
 USE turisticka_agencija;
 
 -- Autor: Mateo Udovčić
-### Mateo Udovčić ###
+
+-- Prikaži 5 korisnika sa najviše danih recenzija, te im se dodaje dodatnih 2000 korisničkih boodva
+
+
+CREATE VIEW recenzijeB AS
+SELECT o.id, o.puno_ime, korisnicki_bodovi, COUNT(r.id_osoba) AS broj_recenzija
+FROM osoba o JOIN recenzija r ON o.id = r.id_osoba
+GROUP BY o.id, o.puno_ime
+ORDER BY broj_recenzija DESC
+LIMIT 5
+;
+
+SELECT id, puno_ime, broj_recenzija, korisnicki_bodovi, korisnicki_bodovi + 2000 AS novi_bodovi
+FROM recenzijeB;
+
+-- Prikaži broj ukupan broj rezervacija u 5. mjesecu od korisnika koji imaju više od 5000 korisničkih bodova
+
+CREATE VIEW broj_rezervacija AS
+SELECT o.puno_ime, o.korisnicki_bodovi, r.vrijeme
+FROM osoba o JOIN rezervacija r ON o.id = r.id_osoba
+WHERE MONTH(r.vrijeme)=5 AND korisnicki_bodovi>5000
+;
+
+SELECT COUNT(*) AS broj_rezervacija_ukupno
+FROM broj_rezervacija 
+;
+
+
+-- Prikaži 3 zaposlenika koja pričaju najviše stranih jezika, dok jedan od tih jezika mora biti njemački
+
+
+CREATE VIEW Njemci AS 
+SELECT  o.id, o.puno_ime
+FROM zaposlenik z JOIN osoba o ON z.id=o.id
+JOIN dodatni_jezik dj ON dj.id_osoba=o.id
+WHERE dj.dodatni_jezik='german'
+;
+
+CREATE VIEW jezici AS
+SELECT Nj.puno_ime, count(dj.dodatni_jezik) AS broj_jezika
+FROM Njemci Nj JOIN dodatni_jezik dj ON Nj.id=dj.id_osoba
+GROUP BY Nj.puno_ime
+ORDER BY broj_jezika DESC
+; 
+
+SELECT *
+FROM jezici
+LIMIT 3
+;
+
+-- Prikaži prosječne ocjene svih transportnih tvrtki kako bi se korisnik mogao odlučiti kojom da putuje, te ih poredaj od bolje prema lošijoj
+
+SELECT t.ime_tvrtke, AVG(r.ocjena) AS prosjecna_ocjena
+FROM transport t
+JOIN recenzija_transporta rt ON t.id = rt.id_transport
+JOIN recenzija r ON rt.id_recenzija = r.id
+GROUP BY t.ime_tvrtke
+ORDER BY prosjecna_ocjena DESC
+;
+
